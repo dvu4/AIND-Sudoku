43ee72f70ebd2dae053bd0cad08a2d56aa32db00
Duc Vu <dvu4@uic.edu>
2017-01-26 23:59:24 -0600

    Add files via upload

--- solution.py
+++ solution.py
@@ -262,7 +262,7 @@ def solve(grid):
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
-    assign_value(solve(diag_sudoku_grid))
+    #assign_value(solve(diag_sudoku_grid))
     
     
     try:
